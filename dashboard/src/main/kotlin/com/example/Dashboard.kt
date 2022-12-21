9c8ece5eafcab4fb6fd479a36c3c483783be123b
René Knop <knopr@haufe.com>
2022-12-21 11:46:31 +0100

    clean-up dashboard

--- dashboard/src/main/kotlin/com/example/Dashboard.kt
+++ dashboard/src/main/kotlin/com/example/Dashboard.kt
@@ -1,38 +1,28 @@
 package com.example
 
 import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ktor.client.engine.cio.*
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
-import kotlinx.serialization.json.*
 
-const val port = 8080
+const val servicePort = 8080
 const val sidecarPort = 3500
 
 fun main() {
-    embeddedServer(Netty, port = port, host = "0.0.0.0", module = Application::module).start(wait = true)
+    embeddedServer(Netty, port = servicePort, host = "0.0.0.0", module = Application::module).start(wait = true)
 }
 
 fun Application.module() {
     this@module.routing {
         get("/dashboard") {
 
-            val client = HttpClient(CIO) {
-                install(ContentNegotiation) {
-                    json(Json {
-                        prettyPrint = true
-                        ignoreUnknownKeys = true
-                    })
-                }
-            }
+            val client = HttpClient(CIO)
 
             val weather = client.get("http://localhost:${sidecarPort}/weather") {
                 contentType(ContentType.Application.Json)
