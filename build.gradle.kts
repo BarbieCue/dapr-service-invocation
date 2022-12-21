297bfc653d866005225402c25a336d2195586a93
René Knop <knopr@haufe.com>
2022-12-21 11:54:21 +0100

    clean-up root build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,8 @@
 plugins {
     kotlin("jvm") version "1.7.22"
     id("io.ktor.plugin") version "2.2.1"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.7.22"
 }
 
-group = "com.example"
-version = "0.0.1"
 application {
     mainClass.set("com.example.ApplicationKt")
 }
